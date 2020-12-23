09ddb00e261c74312c4b9a49418ecfe182bac3cf
ndrohith09 <73429989+ndrohith09@users.noreply.github.com>
2020-12-23 23:17:04 +0530

    second commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -79,7 +79,8 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 DATABASES = {
     'default': {
         #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
+        #'NAME': 'db.sqlite3',
+        #'NAME': 'db.sqlite3',
 
         "ENGINE" : 'djongo',
         'NAME':'shopzap'
